3b49ffbb12953578195c54ad11e4577e6ae7e1a9
xinninggong <xinning.gong@gmail.com>
2021-07-31 15:10:18 +1200

    version 2

--- background.js
+++ background.js
@@ -1,6 +1,22 @@
-let color = '#3aa757';
+// Change the periodInMinutes property to set time interval
+var alarm = chrome.alarms.create("myAlarm", { delayInMinutes: 0.05, periodInMinutes: 20 });
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.alarms.onAlarm.addListener( function (alarm) {
+  
+  // Clear any notification with the same name
+  chrome.notifications.clear('procrastinya-notification')
+  
+  var notification = chrome.notifications.create(
+    'procrastinya-notification', {
+      type: 'basic',
+      iconUrl: "images/gmail.png", // Replace with your own image
+
+      // Customize either the notification title or message
+      title: 'Gmiaol!',
+      message: 'New Emiaol from Meowmeowmeow.'
+    },
+
+    function () {}
+
+  );
 });
