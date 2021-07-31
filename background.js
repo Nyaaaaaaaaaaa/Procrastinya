e3d12478e3c6d0b9e077e9b4673b25fdb766f535
xinninggong <xinning.gong@gmail.com>
2021-07-31 16:18:16 +1200

    fix formatting

--- background.js
+++ background.js
@@ -2,26 +2,26 @@
 var forumUrl = 'https://definetely-not-reddit.netlify.app/';
 var alarm = chrome.alarms.create("myAlarm", { delayInMinutes: 0.05, periodInMinutes: 1 });
 
-chrome.alarms.onAlarm.addListener( function (alarm) {
-  
-  // Clear any notification with the same name
-  chrome.notifications.clear('procrastinya-notification')
-  
-  var notification = chrome.notifications.create(
-    'procrastinya-notification', {
-      type: 'basic',
-      iconUrl: "images/gmail.png", // Replace with your own image
+chrome.alarms.onAlarm.addListener(function (alarm) {
 
-      // Customize either the notification title or message
-      title: 'Gmiaol!',
-      message: 'New Emiaol from DefinitelyNotReddit...'
+    // Clear any notification with the same name
+    chrome.notifications.clear('procrastinya-notification')
+
+    var notification = chrome.notifications.create(
+        'procrastinya-notification', {
+        type: 'basic',
+        iconUrl: "images/gmail.png", // Notification image 
+
+        // Notification title and message
+        title: 'Gmiaol!',
+        message: 'New Emiaol from DefinitelyNotReddit...'
     },
 
-    function () {}
+        function () { }
 
-  );
+    );
 });
 
-chrome.notifications.onClicked.addListener(function(notificationId) {
-    chrome.tabs.create({url: forumUrl});
-  });  
+chrome.notifications.onClicked.addListener(function (notificationId) {
+    chrome.tabs.create({ url: forumUrl });
+});
