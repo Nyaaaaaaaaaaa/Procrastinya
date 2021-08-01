0e358e9e74f99b1830dc58a1b039b15340c153f5
Samuel Chen <68937749+Sparye@users.noreply.github.com>
2021-08-01 14:54:07 +1200

    changed waifu

--- content.js
+++ content.js
@@ -4,3 +4,9 @@ fetch(chrome.runtime.getURL("/model.html"))
     document.body.insertAdjacentHTML("beforeend", html);
     // not using innerHTML as it would break js event listeners of the page
   });
+
+window.proRedirect = () => {
+  window.location.href =
+    "https://definetely-not-reddit.netlify.app" + window.location.pathname;
+};
+// href="https://definetely-not-reddit.netlify.app/"
